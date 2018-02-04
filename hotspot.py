10749eacd127754f27cf6de0a10c67fff8dee902
Muhammad Murtadha Ramadhan <muhmurtadha29@gmail.com>
2018-02-04 22:31:33 +0700

    add hotspot 2015 model

--- hotspot.py
+++ hotspot.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 dataset2014 = pd.read_csv('hotspot_sumatra_2014.csv')
 dataset2015 = pd.read_csv('hotspot_sumatra_2015.csv')
 
+#hotspot 2014
 x_2014 = dataset2014.iloc[:, 0:3].values
 y_2014 = dataset2014.iloc[:, 3].values
 
@@ -35,6 +36,40 @@ y_2014 = y_2014.astype(int)
 
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x_2014, y_2014, test_size = 0.2, random_state = 0)
+
+from sklearn.ensemble import GradientBoostingClassifier
+gbc = GradientBoostingClassifier()
+gbc.fit(x_train, y_train)
+
+gbc.predict(x_test)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, gbc.predict(x_test)))
+
+#hotspot 2015
+x_2015 = dataset2015.iloc[:, 0:3].values
+y_2015 = dataset2015.iloc[:, 3].values
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X1 = LabelEncoder()
+x_2015[:, 0] = labelencoder_X1.fit_transform(x_2015[:, 0])
+labelencoder_X2 = LabelEncoder()
+x_2015[:, 1] = labelencoder_X2.fit_transform(x_2015[:, 1])
+labelencoder_X3 = LabelEncoder()
+x_2015[:, 2] = labelencoder_X3.fit_transform(x_2015[:, 2].astype(str))
+onehotencoder = OneHotEncoder(categorical_features = [0])
+x_2015 = onehotencoder.fit_transform(x_2015).toarray()
+x_2015 = x_2015[:, 1:]
+onehotencoder2 = OneHotEncoder(categorical_features = [22])
+x_2015 = onehotencoder2.fit_transform(x_2015).toarray()
+x_2015 = x_2015[:, 1:]
+
+y_2015[y_2015=="F"]=0
+y_2015[y_2015=="T"]=1
+y_2015 = y_2015.astype(int)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x_2015, y_2015, test_size = 0.2, random_state = 0)
 
 from sklearn.ensemble import GradientBoostingClassifier
 gbc = GradientBoostingClassifier()
